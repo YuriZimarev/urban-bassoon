91ab0232693172cdefdc1b0444575f04e5a9dcad
YuriZimarev <yurvanchik@gmail.com>
2020-12-20 00:13:04 +0400

    Node is chest now

--- safebox/init.lua
+++ safebox/init.lua
@@ -10,9 +10,27 @@ minetest.register_node("safebox:safebox", {
 		"safebox_safebox_front.png"
 	},
 	is_ground_content = false,
-	paramtype2="facedir",
-	groups={cracky=1, level=1}
+	paramtype2 = "facedir",
+	legacy_facedir_simple = true,
+	groups = {cracky=1, level=3,},
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("formspec",
+			"size[8,9]"..
+			default.gui_bg ..
+			default.gui_bg_img ..
+			default.gui_slots ..
+			"list[current_player;safebox:safebox;0,0.3;8,4;]"..
+			"list[current_player;main;0,4.85;8,1;]" ..
+			"list[current_player;main;0,6.08;8,3;8]" ..
+			"listring[current_player;safebox:safebox]" ..
+			"listring[current_player;main]" ..
+			default.get_hotbar_bg(0,4.85)
+		)
+		meta:set_string("infotext", "Safebox")
+	end,
 })
+
 minetest.register_craft({
 	type = "shaped",
 	output = "safebox:safebox 1",
@@ -22,3 +40,7 @@ minetest.register_craft({
 		{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
 	}
 })
+minetest.register_on_joinplayer(function(player)
+	local inv = player:get_inventory()
+	inv:set_size("safebox:safebox", 8*4)
+end)
